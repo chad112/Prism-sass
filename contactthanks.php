0638db74717d83238fe5963c2dedf26b794019a5
chad112 <chad@chadschaub.com>
2014-10-23 11:21:07 -0400

    relink to index
    need to include notify.js

--- contactthanks.php
+++ contactthanks.php
@@ -1,34 +1,38 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
 
 <head>
-	<title>A Nice & Simple Contact Form</title>
-	
-	<link rel="stylesheet" type="text/css" href="style.css" />
+	<!--Typekit -->
+    <script src="//use.typekit.net/qlf1ifk.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
+    <!--end typekit -->
+	<title> PRISM Advertising </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
-
-	<div id="page-wrap">
-
-		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
-		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
-			
-			
-		<br /><br />
-		
-			
-		<h1>Your message has been sent!</h1><br />
-		
-		<p><a href="index.html">Back to Contact Form</a></p>
-	
+	<div class="texture" style="padding-top: -20px;">
 	</div>
-	
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-	</script>
-	<script type="text/javascript">
-	_uacct = "UA-68528-29";
-	urchinTracker();
-	</script>
+
+	<div id="container">
+		<h1> Thank you for your inquiry.</h1>
+	</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
+</script>
+
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')
+</script>
+
+<script>
+$( document ).ready(function() {
+          $("#nav-container"). load("/includes/nav.html");
+        });
+$( document ).ready(function() {
+          $("#nav-thumbs"). load("/includes/gallery.html");
+        });
+$( document ).ready(function() {
+          $("#footer-container"). load("/includes/footer.html");
+        });
+</script>
 
 </body>
 
